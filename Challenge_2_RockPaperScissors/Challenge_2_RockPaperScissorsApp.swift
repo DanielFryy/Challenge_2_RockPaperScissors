78dff74d24c5078624a75603765e05afd257ef7d
Daniel Freire <dgz1606@gmail.com>
2023-12-15 22:02:03 -0500

    Initial Commit

---
+++ Challenge_2_RockPaperScissors/Challenge_2_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2_RockPaperScissorsApp.swift
+//  Challenge_2_RockPaperScissors
+//
+//  Created by Daniel Freire on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge_2_RockPaperScissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge_2_RockPaperScissors
+//
+//  Created by Daniel Freire on 12/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
